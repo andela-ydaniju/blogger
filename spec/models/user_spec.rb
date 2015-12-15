55690b7cf73e5d1905ec82b5af8c35df4a239eaa
andela-ydaniju <yusuf.daniju@andela.com>
2015-12-15 21:46:54 +0100

    create validation for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,30 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  before { @user = User.new(name: "Me", email: "danijuyusuf@mail.com") }
-  subject { @user }
-  it { should respond to(:name) }
-  it { should respond to(:email) }
+  user = User.create!(name: "Me", email: "danijuyusuf@mail.com")
+  context "User is valid" do
+    it "returns true if user is valid" do
+      expect(user.valid?).to be true
+    end
+
+    it "returns false if user is not valid" do
+      user.email = ""
+      expect(user.valid?).to be false
+    end
+
+    it "returns false if user is not valid" do
+      user.name = ""
+      expect(user.valid?).to be false
+    end
+  end
+
+  context "has name and email attributes" do
+    it "has name attribute" do
+      expect(user.name.nil?).to be false
+    end
+
+    it "has email attribute" do
+      expect(user.email.nil?).to be false
+    end
+  end
 end
