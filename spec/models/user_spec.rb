15e7d642d5aa7b9f8f17f5138e8fb925086752e2
andela-ydaniju <yusuf.daniju@andela.com>
2015-12-15 22:20:25 +0100

    Add regex for email matcher

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,37 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   user = User.create!(name: "Me", email: "danijuyusuf@mail.com")
-  context "User is valid" do
+  context "User" do
     it "returns true if user is valid" do
       expect(user.valid?).to be true
     end
 
-    it "returns false if user is not valid" do
+    it "has email which cannot be empty" do
       user.email = ""
       expect(user.valid?).to be false
     end
 
-    it "returns false if user is not valid" do
+    it "has name which cannot be empty" do
       user.name = ""
       expect(user.valid?).to be false
     end
+
+    it "has name of length less than 50" do
+      expect(user.name.length).to be <= 50
+    end
+
+    it "has email of length less than 30" do
+      expect(user.email.length).to be <= 30
+    end
+
+    # it "accepts only valid emails" do
+    #   valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+    #     first.last@foo.jp alice+bob@baz.cn]
+    #   valid_addresses.each do |valid_address|
+    #     user.email = valid_address
+    #     expect(user.valid?).to be true
+    #   end
+    # end
   end
 
   context "has name and email attributes" do
