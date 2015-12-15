3fc1cea242e2133f38723a50d6558ef9add6574e
andela-ydaniju <yusuf.daniju@andela.com>
2015-12-15 14:34:12 +0100

    Setup styling with scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :finances
 
-  root to: "articles#index"
-  get "articles/new"
-  get "users/signin"
-  get "users/index"
+  get "articles/new", to: "articles#new", as: "articles_new"
+  get "users/signin", to: "users#signin"
+  get "users/index", to: "users#index"
 end
